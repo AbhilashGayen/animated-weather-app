07a2d297802f933ae608ba5a4e578eded9c9e9eb
Abhilash Gayen <abhilashgayen@Abhilashs-MacBook-Pro.local>
2020-06-04 12:25:09 +0530

    Updates

---
+++ precache-manifest.a40a601850a50e7ce33e86c13d17a7b4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2dffa37020bf4d88e85f9450123eff63",
+    "url": "/animated-weather-app/index.html"
+  },
+  {
+    "revision": "1a06befe159a9ceabbcf",
+    "url": "/animated-weather-app/static/css/main.7a824294.chunk.css"
+  },
+  {
+    "revision": "8573b5762580850ab7dd",
+    "url": "/animated-weather-app/static/js/2.bc55ff06.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/animated-weather-app/static/js/2.bc55ff06.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a06befe159a9ceabbcf",
+    "url": "/animated-weather-app/static/js/main.4a4f6433.chunk.js"
+  },
+  {
+    "revision": "f1f15401a04299c6a8f3",
+    "url": "/animated-weather-app/static/js/runtime-main.7c7f6b70.js"
+  },
+  {
+    "revision": "f4106b1911fc9423ea9047b5bb19687e",
+    "url": "/animated-weather-app/static/media/rain.f4106b19.png"
+  },
+  {
+    "revision": "78ea2c4f82d283109f464d3e36deb238",
+    "url": "/animated-weather-app/static/media/weather-lighthouse.78ea2c4f.png"
+  }
+]);
